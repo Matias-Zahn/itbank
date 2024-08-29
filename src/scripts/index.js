73b7d759931c3eb2fa9cc254ff66144b9e0896cd
Matias Zahn <zahnmatias14@gmail.com>
2024-08-28 22:39:48 -0300

    Modulo transferencia

---
+++ src/scripts/index.js
@@ -0,0 +1,16 @@
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = new FormData(form);
+
+  let formDataObj = {};
+
+  formData.forEach((value, index) => {
+    formDataObj[index] = value;
+  });
+
+  let formJson = JSON.stringify(formDataObj);
+
+  localStorage.setItem("formTransfer", formJson);
+});
